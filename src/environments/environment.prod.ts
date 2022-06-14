d3700a6b38c8ee96932d9d4edece8516eb03fb9b
luan10017 <luan10017@hotmail.com>
2022-06-14 15:35:46 -0300

    Apontamento back-end aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: "https://smart-service-back-end.herokuapp.com/"
-  // API: "http://52.0.30.216:8080/"
+  // API: "https://smart-service-back-end.herokuapp.com/"
+  API: "http://52.0.30.216:8080/"
 };
